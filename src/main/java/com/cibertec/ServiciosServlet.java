f961391c235ff63e7544f4652dcd9b8d24ca604f
SUITE <dael202106@gmail.com>
2024-11-23 13:20:36 -0500

    Estructura Maven

---
+++ src/main/java/com/cibertec/ServiciosServlet.java
@@ -0,0 +1,10 @@
+package main.java.com.cibertec;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/ServiciosServlet")
+public class ServiciosServlet extends HttpServlet {
+
+
+}
